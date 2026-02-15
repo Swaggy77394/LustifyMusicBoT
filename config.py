78ddf1219368e5459c793aca47b84690e82cecb5
Swaggy77394 <abhishek.7739476399@gmail.com>
2026-02-15 08:47:53 +0530

    Update image URLs in config.py

--- config.py
+++ config.py
@@ -95,11 +95,11 @@ confirmer = {}
 
 START_IMG_URL = getenv(
     "START_IMG_URL",
-    "https://files.catbox.moe/34xlvu.jpg",
+    "https://files.catbox.moe/aov76u.jpg",
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://files.catbox.moe/34xlvu.jpg",
+    "https://files.catbox.moe/aov76u.jpg",
 )
 PLAYLIST_IMG_URL = "https://files.catbox.moe/aov76u.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/aov76u.jpg"
